adbcbdf74c94307206c2528e081bc6980289eda4
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2024-03-24 15:18:46 +0630

    create user and roles modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, TeacherModule, AdminModule, StudentModule],
   controllers: [],
   providers: [],
 })
