407151c27ef73a1b9b2a6c8ec146e667ea12e90c
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2024-03-30 14:49:16 +0630

    setup global error handler

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './auth/exception-filter/allExceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
   await app.listen(3000);
 }
 bootstrap();
