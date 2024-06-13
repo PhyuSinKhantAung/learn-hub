a8f14af00d1584f44bc6af6daf8a882e1c39ebd7
PhyuSin <rollyphyusinkhant7290@gmail.com>
2024-06-13 16:43:59 +0630

    added get me route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { GetUser } from 'src/auth/decorator';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('/me')
+  async getMe(@GetUser('id') userId: number) {
+    const user = await this.userService.getUserById(userId);
+
+    if (user.id !== userId)
+      throw new UnauthorizedException('You are not authorized.');
+
+    return user;
+  }
+}
